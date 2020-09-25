84e235fd83015e38d38df89c37e419f38979ff99
Lucas Polizeli <lumpolizeli@gmail.com>
2020-09-24 21:11:32 -0300

    feat: add screenshot api list lambda

---
+++ functions/screenshot/list.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+
+const dynamoDbConnection = new AWS.DynamoDB.DocumentClient();
+
+module.exports.handle = (event, _context, callback) => {
+  const userId = event.pathParameters.userId;
+
+  const params = {
+    TableName: process.env.SCREENSHOT_TABLE,
+    FilterExpression: "#userId = :userId",
+    ExpressionAttributeNames: {
+        "#userId": "userId",
+    },
+    ExpressionAttributeValues: { ":userId": userId }
+  }
+
+  dynamoDbConnection.scan(params, (error, result) => {
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: 500,
+        headers: { 'Content-Type': 'text/plain' },
+        body: "Couldn't find the screenshot item",
+      });
+      return;
+    }
+
+    const response = {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify(result.Items),
+    };
+    callback(null, response);
+  });
+};
