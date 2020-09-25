b8560212e1ac6f89b8198b33883e99204462aacc
Barreto <v.barreto.and@gmail.com>
2020-09-24 21:11:38 -0300

    feat: 🎸 add about functionality

---
+++ functions/about/show.js
@@ -0,0 +1,15 @@
+"use strict";
+
+module.exports.handle = (_event, _context, callback) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      version: "1.0.1",
+    }),
+  };
+  callback(null, response);
+};
